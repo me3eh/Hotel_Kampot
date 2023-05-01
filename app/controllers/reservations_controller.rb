06fe53992a07ae9f2936087074e75167b9c446de
me3eh <matt30002000@gmail.com>
2023-05-02 01:16:06 +0200

    Add action to controller, callback to it and new migratino

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,12 +3,19 @@
 class ReservationsController < ApplicationController
   soap_service namespace: 'urn:WashOut'
 
+  before_action :dump_parameters
+  def dump_parameters
+    puts " asdfasdfasdfasdfasfasdf\n\n\n\n\n\nasdfasdfasfasdfasdfasdfasdf"
+  end
   # Simple case
   soap_action "list",
               :args   => nil,
               :return => { list: { values: [{id: :string, room_number: :integer, from: :string, to: :string}] } }
   def list
-    f = Reservation.all.map do |res|
+    open_reservations = Reservation.where(uuid: nil)
+    raise SOAPError, "There are no reservations left" if open_reservations.empty?
+
+    f = open_reservations.map do |res|
       res.attributes
     end
 
@@ -21,29 +28,39 @@ class ReservationsController < ApplicationController
               :args   => { :id => :string },
               :return => { pdf: :string, pdf_name: :string }
   def get_pdf
-    binding.pry
+    reservation = Reservation.find_by(id: params[:id])
     uuid = SecureRandom.uuid
+    reservation.update(uuid: uuid)
     filename = "#{uuid}.pdf"
-    id_for_pdf = "#{params[:id]}"
 
       ::Prawn::Document.generate("tmp/#{filename}") do
-        text "Hello World!"
-        text id_for_pdf
+        text "Hello there",
+             size: 36, align: :center, leading: 2
+        text "Room number: #{reservation.id}"
+        text "From: #{reservation.from}"
+        text "To: #{reservation.to}"
+        text "REMEMBER - your reservation name: #{reservation.uuid}"
       end
 
     file_data = Base64.encode64(File.binread("tmp/#{filename}"))
     render :soap => { pdf: (file_data), pdf_name: filename }
   end
 
-  soap_action "integers_to_boolean",
-              :args => nil,
-              :as => 'MyRequest',
-              :return => [:boolean]
+  soap_action "get_reservation_by_code",
+              :args   => { :id => :string },
+              :return => { value: {id: :string, room_number: :integer, from: :string, to: :string} }
+  def get_reservation_by_code
+    reservation = Reservation.find_by(uuid: params[:id])
+    raise SOAPError, "There is no reservation with such uuid" if reservation.nil?
 
-  # You can use all Rails features like filtering, too. A SOAP controller
-  # is just like a normal controller with a special routing.
-  # before_filter :dump_parameters
-  # def dump_parameters
-  #   Rails.logger.debug params.inspect
-  # end
+    reservation_hash =
+      {
+        id: reservation.id,
+        room_number: reservation.room_number,
+        from: reservation.from,
+        to: reservation.to
+      }
+
+    render soap: { value: reservation_hash }
+  end
 end
